71f82ef0b2ca24757b4b7812d1188ecb6c932a4d
Yordi de Kleijn <yordi@temper.works>
2017-08-03 09:06:02 +0200

    Update references

--- build/options.js
+++ build/options.js
@@ -4,11 +4,6 @@ const path = require('path');
 const version = require('../package.json').version;
 
 const banner = '';
-  // '/*!\n' +
-  // ' * Nucleus UI Kit v' + version + ' (https://github.com/JosephusPaye/keen-ui)\n' +
-  // ' * (c) ' + new Date().getFullYear() + ' Josephus Paye II\n' +
-  // ' * Released under the MIT License.\n' +
-  // ' */';
 
 module.exports = {
     banner,
