1011f2ae5ec031d805e72b64b5000584fb42c658
Milroy Fraser <milroy@outlook.com>
2017-12-07 11:00:35 +0530

    icon issue fixed and updated docs

--- src/UiSelect.vue
+++ src/UiSelect.vue
@@ -23,12 +23,6 @@
                 ref="label"
 
                 :tabindex="disabled ? null : '0'"
-
-                @click="toggleDropdown"
-                @focus="onFocus"
-                @keydown.enter.prevent="openDropdown"
-                @keydown.space.prevent="openDropdown"
-                @keydown.tab="onBlur"
             >
                 <div
                     class="ui-select__label-text"
@@ -42,17 +36,29 @@
                     <div
                         class="ui-select__display-value"
                         :class="{ 'is-placeholder': !hasDisplayText }"
+
+                        @click="toggleDropdown"
+                        @focus="onFocus"
+                        @keydown.enter.prevent="openDropdown"
+                        @keydown.space.prevent="openDropdown"
+                        @keydown.tab="onBlur"
                     >
                         {{ hasDisplayText ? displayText : (hasFloatingLabel && isLabelInline) ? null : placeholder }}
                     </div>
 
-
+                    <ui-icon
+                            v-show="showDropdownIcon"
+                            class="ui-select__dropdown-button"
+                            @click.native="toggleDropdown"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="10">
+                            <path d="M.9 2.85L7.94 9.9l7.07-7.05c.5-.5.5-1.3 0-1.8-.5-.48-1.3-.48-1.8 0l-5.27 5.3-5.27-5.3c-.5-.48-1.3-.48-1.8 0-.48.5-.48 1.3 0 1.8"/>
+                        </svg>
+                    </ui-icon>
     
                     <ui-icon v-show="showClearIcon" class="ui-select__dropdown-button">
-                        <svg @click="reset()" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27" width="14" height="14">
-                            <path d="M18,19c-0.3,0-0.5-0.1-0.7-0.3l-18-18c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l18,18c0.4,0.4,0.4,1,0,1.4C18.5,18.9,18.3,19,18,19
-	z"/>
-                            <path d="M0,19c-0.3,0-0.5-0.1-0.7-0.3c-0.4-0.4-0.4-1,0-1.4l18-18c0.4-0.4,1-0.4,1.4,0s0.4,1,0,1.4l-18,18C0.5,18.9,0.3,19,0,19z"/>
+                        <svg @click="clear()" v-show="showClearIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27" width="14" height="14">
+                            <path d="M18.984 6.422L13.406 12l5.578 5.578-1.406 1.406L12 13.406l-5.578 5.578-1.406-1.406L10.594 12 5.016 6.422l1.406-1.406L12 10.594l5.578-5.578z"/>
                         </svg>
                     </ui-icon>
                     
@@ -237,7 +243,8 @@ export default {
         disabled: {
             type: Boolean,
             default: false
-        }
+        },
+        clearable: Boolean
     },
 
     data() {
@@ -299,10 +306,18 @@ export default {
         },
 
         showClearIcon() {
+            if (! this.clearable) {
+                return false;
+            }
+
             return !this.disabled && this.value.length;
         },
 
         showDropdownIcon() {
+            if (! this.clearable) {
+                return true;
+            }
+
             return !this.showClearIcon;
         },
 
@@ -579,7 +594,15 @@ export default {
         },
 
         reset() {
-            this.setValue(JSON.parse(this.initialValue));
+            this.resetTo(JSON.parse(this.initialValue));
+        },
+
+        clear() {
+            this.resetTo("");
+        },
+
+        resetTo(value) {
+            this.setValue(value);
             this.clearQuery();
             this.resetTouched();
 
